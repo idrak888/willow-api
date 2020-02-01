d6bf32b5d928cf0deb382d01dbb8bb4b0b095b60
Idrak Mustahsin <playbox8g@gmail.com>
2020-02-01 18:35:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post("/user", (req, res) => {
     });
 });
 
-app.post("/notication/:username", (req, res) => {
+app.post("/notification/:username", (req, res) => {
 	var username = req.params.username;
 	
 	User.findOneAndUpdate({ username }, { $push: { notifications:req.body.notification } }).then(doc => {
