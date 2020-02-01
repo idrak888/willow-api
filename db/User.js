39767fdc335b94748743989f5284260c6bfdd36f
Idrak Mustahsin <playbox8g@gmail.com>
2020-02-01 18:23:01 +0800

    Update User.js

--- db/User.js
+++ db/User.js
@@ -3,7 +3,8 @@ const {mongoose} = require('./mongoose');
 const User = mongoose.model('User', {
     _id: String,
 	username: { type: String, unique: true },
-    email: String
+    email: String,
+	notifications: Array
 });
 
 module.exports = {
